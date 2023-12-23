1dd929585c1d973be9b45da8f623a1aed05a0b2d
Thanh Nguyen <nguyentuthanh032002@gmail.com>
2023-12-23 09:47:10 +0700

    feat: fix

--- src/pages/user/ServiceDetail.js
+++ src/pages/user/ServiceDetail.js
@@ -28,9 +28,30 @@ export default function ServiceDetail() {
   };
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
-  useEffect(() => {
     getInforNew();
+    if (window.FB) {
+      window.FB.XFBML.parse();
+    }
+    let locale = "vi_VN";
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: "1069091167572595", // You App ID
+        cookie: true, // enable cookies to allow the server to access
+        // the session
+        xfbml: true, // parse social plugins on this page
+        version: "v2.1", // use version 2.1
+      });
+    };
+    // Load the SDK asynchronously
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = `//connect.facebook.net/${locale}/sdk.js`;
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
   }, [id]);
 
   return (
